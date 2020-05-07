883a96fed9949d04dcf34ffe4a1a0b066afc2331
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-05-07 11:12:46 +0900

    add Do()

--- main.go
+++ main.go
@@ -2,25 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
+	"./greeting"
 )
 
 func main() {
-	// 現在時刻
-	t := time.Now().UnixNano()
-	rand.Seed(t)
-	n := rand.Intn(6) // 0-5
-	fmt.Println(t, n)
-
-	switch n + 1 {
-	case 6:
-		fmt.Println("大吉")
-	case 5, 4:
-		fmt.Println("中吉")
-	case 3, 2:
-		fmt.Println("小吉")
-	default:
-		fmt.Println("凶")
-	}
+	greet := greeting.Do()
+	fmt.Println(greet)
 }
