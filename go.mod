a30c223ef357dc2cc5622a23c722055ae9237718
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-05-08 23:45:12 +0900

    add go mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/hoge/hello
+
+go 1.14
+
+require rsc.io/quote/v2 v2.0.1
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-	Mygreet "./greeting"
-	"github.com/tenntenn/greeting"
+    "fmt"
+    "rsc.io/quote/v2"
 )
 
 func main() {
-	greet := Mygreet.Do()
-	fmt.Println(greet, greeting.Do())
+    fmt.Println(quote.HelloV2())
 }
