cf446bf1e27e98788cd3f975f67c2ba9bdc16a58
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-05-09 18:05:16 +0900

    add args

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-    "fmt"
+		"fmt"
+		"os"
+		"flag"
+		"strings"
+
     "rsc.io/quote/v2"
 )
 
+var msg = flag.String("msg", "defalt", "説明")
+var n int
+
+func init() {
+	flag.IntVar(&n, "n", 1, "回数")
+}
+
 func main() {
-    fmt.Println(quote.HelloV2())
+		fmt.Println(quote.HelloV2())
+		flag.Parse()
+		fmt.Println(strings.Repeat(*msg, n))
+		fmt.Println(os.Args)
 }
