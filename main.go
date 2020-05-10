87e9c4ccd4d57535ddba0a2ba2fff6c91554ec43
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-05-10 21:58:24 +0900

    fix channel

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ func makeCh() chan int {
 	return make(chan int)
 }
 
-func recvCh(recv chan int) int {
-	return <-recv *2
+func recvCh(recv <-chan int) int {
+	return <-recv
 }
 
 func main() {
 	ch := makeCh()
-	go func() { ch <- 100 }()
+	go func(ch chan<- int) { ch <- 100 }(ch)
 	fmt.Println(recvCh(ch))
 }
