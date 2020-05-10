ddbe70bf078e6559b64f43f95960ed8dd0a0597e
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-05-10 21:15:54 +0900

    add go routine

--- main.go
+++ main.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-		"fmt"
-		"os"
-		"flag"
-		"strings"
-
-    "rsc.io/quote/v2"
+	"fmt"
+	"time"
 )
 
-var msg = flag.String("msg", "defalt", "説明")
-var n int
-
-func init() {
-	flag.IntVar(&n, "n", 1, "回数")
-}
-
 func main() {
-		fmt.Println(quote.HelloV2())
-		flag.Parse()
-		fmt.Println(strings.Repeat(*msg, n))
-		fmt.Println(os.Args)
+	go func() {
+		fmt.Println("別のゴールーチン")
+	}()
+	fmt.Println("mainゴールーチン")
+	time.Sleep(50 * time.Millisecond)
 }
