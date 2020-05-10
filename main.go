f49ebd5540780ab4e969e73ee7494c14444adfc3
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-05-10 21:36:45 +0900

    add chanel

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	go func() {
-		fmt.Println("別のゴールーチン")
-	}()
-	fmt.Println("mainゴールーチン")
-	time.Sleep(50 * time.Millisecond)
+	ch1 := make(chan int)
+	var ch2 chan string
+	go func() { ch1 <- 100 }()
+	go func() { ch2 <- "hi" }()
+
+	select {
+	case v1 := <-ch1:
+		fmt.Println(v1)
+	case v2 := <-ch2:
+		fmt.Println(v2)
+	}
 }
