dab9dce4ee53078339507b9973a9c345626b4e03
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-05-19 00:23:59 +0900

    add handler

--- main.go
+++ main.go
@@ -1,31 +1,47 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"log"
+	"html/template"
+	"math/rand"
+	"net/http"
+	"time"
 )
 
+var tmpl = template.Must(template.New("msg").
+	Parse("<html><body>{{.Name}}さんの運勢は「<b>{{.Omikuji}}</b>」です</body></html>"))
+
+type Result struct {
+	Name    string
+	Omikuji string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	result := Result{
+		Name:    r.FormValue("p"),
+		Omikuji: omikuji(),
+	}
+	tmpl.Execute(w, result)
+}
+
 func main() {
-	type Person struct {
-		Name string `json:"name"`
-		Age  int    `json:"age"`
-	}
-	p := &Person{Name: "wataru", Age: 21}
+	// 現在時刻
+	t := time.Now().UnixNano()
+	rand.Seed(t)
 
-	var buf bytes.Buffer
-	enc := json.NewEncoder(&buf)
-	if err := enc.Encode(p); err != nil {
-		log.Fatal(err)
-	}
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
 
-	fmt.Println(buf.String())
-
-	var p2 Person
-	dec := json.NewDecoder(&buf)
-	if err := dec.Decode(&p2); err != nil {
-		log.Fatal(err)
+func omikuji() string {
+	n := rand.Intn(6) // 0-5
+	switch n + 1 {
+	case 6:
+		return "大吉"
+	case 5, 4:
+		return "中吉"
+	case 3, 2:
+		return "小吉"
+	default:
+		return "凶"
 	}
-	fmt.Println(p2)
 }
